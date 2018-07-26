675ca8ad923c6d36c6b026e5d3e3d0cc9e41bba9
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-26 18:47:43 +0700

    Design dong_item

--- app/src/main/java/com/example/duongtainhan555/sqlite/NhaHang.java
+++ app/src/main/java/com/example/duongtainhan555/sqlite/NhaHang.java
@@ -1,5 +1,47 @@
 package com.example.duongtainhan555.sqlite;
 
 public class NhaHang {
+    private Integer id;
+    private String tenmonan;
+    private Integer giamonan;
+    private String diadiem;
 
+    public NhaHang(Integer id, String tenmonan, Integer giamonan, String diadiem) {
+        this.id = id;
+        this.tenmonan = tenmonan;
+        this.giamonan = giamonan;
+        this.diadiem = diadiem;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTenmonan() {
+        return tenmonan;
+    }
+
+    public Integer getGiamonan() {
+        return giamonan;
+    }
+
+    public String getDiadiem() {
+        return diadiem;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTenmonan(String tenmonan) {
+        this.tenmonan = tenmonan;
+    }
+
+    public void setGiamonan(Integer giamonan) {
+        this.giamonan = giamonan;
+    }
+
+    public void setDiadiem(String diadiem) {
+        this.diadiem = diadiem;
+    }
 }
